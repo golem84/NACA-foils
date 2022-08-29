32231dda824b0ab98f06b7e6dafa5cd6547c54e6
golem84 <andr0073@yandex.ru>
2022-08-29 23:54:27 +0500

    fixing files

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textProfile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textChord = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textAngle = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.btnGraph = new System.Windows.Forms.Button();
+            this.textNPoint = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtz = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textProfile
+            // 
+            this.textProfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textProfile.Location = new System.Drawing.Point(13, 13);
+            this.textProfile.Margin = new System.Windows.Forms.Padding(4);
+            this.textProfile.MaxLength = 4;
+            this.textProfile.Name = "textProfile";
+            this.textProfile.Size = new System.Drawing.Size(91, 24);
+            this.textProfile.TabIndex = 0;
+            this.textProfile.Text = "0012";
+            this.textProfile.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(126, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Номер профиля, ХХХХ";
+            // 
+            // textChord
+            // 
+            this.textChord.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textChord.Location = new System.Drawing.Point(13, 45);
+            this.textChord.Margin = new System.Windows.Forms.Padding(4);
+            this.textChord.MaxLength = 6;
+            this.textChord.Name = "textChord";
+            this.textChord.Size = new System.Drawing.Size(91, 24);
+            this.textChord.TabIndex = 1;
+            this.textChord.Text = "25.4";
+            this.textChord.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(126, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Длина хорды, мм";
+            // 
+            // textAngle
+            // 
+            this.textAngle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textAngle.Location = new System.Drawing.Point(13, 109);
+            this.textAngle.Margin = new System.Windows.Forms.Padding(4);
+            this.textAngle.MaxLength = 6;
+            this.textAngle.Name = "textAngle";
+            this.textAngle.Size = new System.Drawing.Size(91, 24);
+            this.textAngle.TabIndex = 3;
+            this.textAngle.Text = "3.5";
+            this.textAngle.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(126, 112);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Угол атаки, град";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(15, 179);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(110, 53);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Рассчитать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(186, 179);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(110, 53);
+            this.btnOutput.TabIndex = 7;
+            this.btnOutput.Text = "Вывести в файл";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // btnGraph
+            // 
+            this.btnGraph.Location = new System.Drawing.Point(357, 48);
+            this.btnGraph.Name = "btnGraph";
+            this.btnGraph.Size = new System.Drawing.Size(56, 53);
+            this.btnGraph.TabIndex = 7;
+            this.btnGraph.UseVisualStyleBackColor = true;
+            // 
+            // textNPoint
+            // 
+            this.textNPoint.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textNPoint.Location = new System.Drawing.Point(13, 141);
+            this.textNPoint.Margin = new System.Windows.Forms.Padding(4);
+            this.textNPoint.MaxLength = 4;
+            this.textNPoint.Name = "textNPoint";
+            this.textNPoint.Size = new System.Drawing.Size(91, 24);
+            this.textNPoint.TabIndex = 4;
+            this.textNPoint.Text = "50";
+            this.textNPoint.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(126, 144);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Количество точек";
+            // 
+            // txtz
+            // 
+            this.txtz.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtz.Location = new System.Drawing.Point(13, 77);
+            this.txtz.Margin = new System.Windows.Forms.Padding(4);
+            this.txtz.MaxLength = 6;
+            this.txtz.Name = "txtz";
+            this.txtz.Size = new System.Drawing.Size(91, 24);
+            this.txtz.TabIndex = 2;
+            this.txtz.Text = "20";
+            this.txtz.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(126, 80);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(182, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Положение по оси Z, мм";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(443, 247);
+            this.Controls.Add(this.btnGraph);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textNPoint);
+            this.Controls.Add(this.textAngle);
+            this.Controls.Add(this.txtz);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textChord);
+            this.Controls.Add(this.textProfile);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "NACA 4-digit foil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textProfile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textChord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textAngle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.Button btnGraph;
+        private System.Windows.Forms.TextBox textNPoint;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtz;
+        private System.Windows.Forms.Label label5;
+
+
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        // переменные для расчетов
+        double Chord, Angle, zz;
+        int NPoint;
+        string txtProfile;
+        bool isCenter;
+        // массивы значений для точек кривых
+        double[] high_x = new double [250];
+        double[] high_y = new double[250];
+        double[] low_x = new double[250];
+        double[] low_y = new double[250];
+        double[] c = new double[250];
+        double[] x = new double[250];
+
+        double cx0, cy0, mass; // координаты центра тяжести, масса фигуры
+
+        string mes1, mes2, mes3;
+
+        /*
+        Array high_x = new double[250]; // верхняя дуга профиля
+        Array high_y = new double[250];
+        Array low_x = new double[250]; // нижняя дуга профиля
+        Array low_y = new double[250];
+        Array c = new double[250]; // координата средней линии
+        Array x = new double[250]; // координата Х
+        //Array yt = new double[250]; // толщина профиля
+        //Array dc = new double[250]; // производная от средней линии
+        */
+        string fn; // имя файла
+        
+
+        // обработка событий формы
+        public Form1()
+        {
+            InitializeComponent();
+          
+           // инициализация массива значений для кривых
+            /*
+            high_x.Initialize();
+            low_x.Initialize();
+            high_y.Initialize();
+            low_y.Initialize();
+
+            c.Initialize();
+            x.Initialize();
+            //dc.Initialize();
+            */
+            saveFileDialog1.DefaultExt = "txt";
+            saveFileDialog1.Filter = "текст|*.txt";
+            saveFileDialog1.Title = "Сохранение файла.";
+            fn = "";
+            
+        }
+
+        private int SaveDocument()
+        {
+            int result = 0;
+            
+           
+                // отобразить диалог Сохранить
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    // отобразить имя файла в заголовке окна
+                    fn = saveFileDialog1.FileName;
+                    // this.Text = fn;
+                }
+                
+
+            // сохранить файл
+            if (fn != string.Empty)
+            {
+                try
+                {
+                    // получим информацию о файле fn
+                    System.IO.FileInfo fi = new System.IO.FileInfo(fn);
+                    // поток для записи (перезаписываем файл)
+                    System.IO.StreamWriter sw = fi.CreateText();
+                    // записываем данные
+
+                    NumberFormatInfo myformat = new CultureInfo( "en-US", false ).NumberFormat;
+                    myformat.NumberDecimalSeparator = ".";
+
+                    double a = zz;
+                    for (int i = NPoint; i >= 0; i--)
+                    {
+                        
+                        //a = Convert.ToDouble(high_x.GetValue(i));
+                        sw.Write(high_x[i].ToString("000.000", myformat ) + "mm");
+                        sw.Write('\t'); // знак табуляции между числами
+                        //a = Convert.ToDouble(high_y.GetValue(i));
+                        sw.Write(high_y[i].ToString("000.000", myformat) + "mm");
+                        sw.Write('\t'); // знак табуляции между числами
+                        //a = zz;
+                        sw.WriteLine(a.ToString("000.000", myformat) + "mm");
+                    }
+                    
+                    for (int i = 1; i <= NPoint; i++)
+                    {
+                        
+                        //a = Convert.ToDouble(low_x.GetValue(i));
+                        sw.Write(low_x[i].ToString("000.000", myformat) + "mm");
+                        sw.Write('\t'); // знак табуляции между числами
+                        //a = Convert.ToDouble(low_y.GetValue(i));
+                        sw.Write(low_y[i].ToString("000.000", myformat) + "mm");
+                        sw.Write('\t'); // знак табуляции между числами
+                        //a = zz;
+                        sw.WriteLine(a.ToString("000.000", myformat) + "mm");
+                    }
+
+                   // sw.WriteLine(mes1);
+                   // sw.WriteLine(mes2);
+                   // sw.WriteLine(mes3);
+                    // закрываем поток
+                    sw.Close();
+                    result = 0;
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.ToString(), "NkEdit",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            return result;
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            SaveDocument();
+            
+        }
+                
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // проверка введенных данных
+            try
+            {
+                Chord = Convert.ToDouble(textChord.Text);
+                Angle = -Convert.ToDouble(textAngle.Text)*3.14159265/180;
+                NPoint = Convert.ToInt16(textNPoint.Text);
+                zz = Convert.ToDouble(txtz.Text);
+
+                txtProfile = textProfile.Text;
+            }
+            catch (FormatException ex)
+            {
+                if ((textChord.Text.Length == 0) || (textAngle.Text.Length == 0) || (textNPoint.Text.Length == 0) || (textProfile.Text.Length == 0))
+                    MessageBox.Show("Заполните пустые поля." , "Внимание! "+ ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                else
+                    MessageBox.Show("Ошибка в полях." , "Ошибка! "+ ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (NPoint >= 250) MessageBox.Show("Задайте меньшее количество расчетных точек.", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            // перевод угла из радиан в градусы
+            // положительное направление - вращение системы координат по часовой стрелке
+            //Angle = -Angle * 180 / 3.14159265; 
+
+            // нахождение параметров профиля
+            double pos, camber, thik, t;
+            int p;
+            p = Convert.ToInt32(txtProfile);
+            camber = (double) (p / 1000) * 0.01; // сред линия
+            pos = (double) (p % 1000 / 100) * 0.1; // положение макс толщины
+            thik = (double)(p % 100) * 0.01; // толщина профиля
+            double pos1 = pos * Chord;          
+          
+                // промежуточные переменные для координат
+                double xc, yc; 
+                double ee;
+                double dcc;
+                double teta;
+                double yh, yl, xh, xl;
+            // нахождение координат верхней и нижней части профиля
+            for (int i = 0; i <= NPoint; i++)
+            {
+                xc = Chord * i / NPoint; // координата Х
+                // координата У для средней линии
+                ee = xc / Chord;
+                if (xc < pos1) yc = camber * xc * (2 * pos - ee) / (pos * pos);
+                else if (pos1==0) yc=0;
+                else yc = camber * (Chord -xc) * (1+ee-2*pos) / ((1 - pos) * (1 - pos));
+                
+                // толщины профиля
+                t = 5 * thik* Chord *(0.2969*Math.Sqrt(ee)-0.126*ee-0.3516*ee*ee+0.2843*ee*ee*ee-0.1015*ee*ee*ee*ee);
+                // производная в точке Х
+                if (xc < pos1) dcc = 2 * camber * (1 - ee / pos) / pos;
+                else if (pos1 == 0) dcc = 2 * camber;
+                else dcc = 2 * camber * (pos - ee) / ((1 - pos) * (1 - pos));
+                teta = Math.Atan(dcc);
+                // верхняя и нижняя кромки профиля
+                xh = xc - t * Math.Sin(teta);
+                xl = xc + t * Math.Sin(teta);
+                yh = yc + t * Math.Cos(teta);
+                yl = yc - t * Math.Cos(teta);
+                // записываем результы расчета в массив
+                c[i] = yc; // координата средней линии в массив
+                x[i] = xc; // координата Х в массив
+                //yt.SetValue(t, i);
+                high_x[i]=xh;
+                high_y[i] = yh;
+                low_x[i] = xl;
+                low_y[i] = yl;  
+            }
+            
+            // вычисляем центр масс фигуры
+            double dx, dy, ss1, ss2;
+            double cxs, cys, ss;
+            double cx1, cy1, cx2, cy2;
+            // обнуляем сумматоры
+            cx1 = 0;
+            cx2 = 0;
+            cy1 = 0;
+            cy2 = 0;
+            ss1 = 0;
+            ss2 = 0;
+            for (int i=0; i<=NPoint-1; i++)
+            {
+                // координаты центра масс элементарной трапеции
+                dx = high_x[i + 1] - high_x[i];
+                dy = high_y[i + 1] - high_y[i];
+                ss = dx * high_y[i] + 0.5 * dx * dy;
+                cxs = (high_x[i] + 0.5 * dx) * dx * high_y[i] + 0.5 * dx * dy * (high_x[i] + 2 * dx / 3);
+                cys = 0.5 * high_y[i] * high_y[i] * dx + 0.5 * dx * dy * (high_y[i] + dy / 3);
+                
+                ss1 = ss1 + ss;
+                cx1 = cx1 + cxs;
+                cy1 = cy1 + cys;
+
+                dx = low_x[i + 1] - low_x[i];
+                dy = low_y[i + 1] - low_y[i];
+                ss = dx * low_y[i] + 0.5 * dx * dy;
+                cxs = (low_x[i] + 0.5 * dx) * dx * low_y[i] + 0.5 * dx * dy * (low_x[i] + 2 * dx / 3);
+                cys = 0.5 * low_y[i] * low_y[i] * dx + 0.5 * dx * dy * (low_y[i] + dy / 3);
+                
+                ss2 = ss2 + ss;
+                cx2 = cx2 + cxs;
+                cy2 = cy2 + cys;
+            }
+            
+            /*
+            mes1 = Convert.ToString(ss1) + ' ' + Convert.ToString(cx1) + ' ' + Convert.ToString(cy1);
+            mes2 = Convert.ToString(ss2) + ' ' + Convert.ToString(cx2) + ' ' + Convert.ToString(cy2);
+            mes3 = Convert.ToString(ss1-ss2)+' '+Convert.ToString((cx1*ss1-cx2*ss2)/(ss1-ss2))+' '+Convert.ToString((cy1*ss1-cy2*ss2)/(ss1-ss2));
+             */
+            mass = ss1 - ss2;
+            cx0 = (cx1  - cx2 ) / mass;
+            cy0 = (cy1  - cy2 ) / mass;
+
+            // переносим начало координат в центр масс профиля
+            for (int i = 0; i<=NPoint; i++)
+            {
+                high_x[i] = high_x[i] - cx0;
+                low_x[i] = low_x[i] - cx0;
+                high_y[i] = high_y[i] - cy0;
+                low_y[i] = low_y[i] - cy0;
+            }
+
+            // поворачиваем оси координат на заданный угол
+
+            for (int i = 0; i <= NPoint; i++)
+            {
+                dx = high_x[i] * Math.Cos(Angle) + high_y[i] * Math.Sin(Angle);
+                dy = -high_x[i] * Math.Sin(Angle) + high_y[i] * Math.Cos(Angle);
+                high_x[i] = dx;
+                high_y[i] = dy;
+
+                dx = low_x[i] * Math.Cos(Angle) + low_y[i] * Math.Sin(Angle);
+                dy = -low_x[i] * Math.Sin(Angle) + low_y[i] * Math.Cos(Angle);
+                low_x[i] = dx;
+                low_y[i] = dy;
+            }
+
+        }
+    }
+}
